569ba5c7c4a454a8e25ad39b01e3ff1b78660cc2
Kartavya Bang <bangkartavya@gmail.com>
2023-12-18 09:12:39 +0530

    12/18/2023

---
+++ Game of XOR/main.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int gameOfXor(int N , int A[]) {
+        if(N%2==0) return 0;
+        int c=A[0];
+        int i=2;
+        
+        while(i<N) {
+            c=c^A[i];
+            i+=2;
+        }
+        return c;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int A[N];
+        for(int i=0 ; i<N ; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.gameOfXor(N,A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
