214552e3a285a3bc0e4caa81f546a78e6aa9cc88
Kartavya Bang <bangkartavya@gmail.com>
2023-12-19 09:06:33 +0530

    12/19/2023

---
+++ Rightmost Different Bit/main.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+        if(m==n) return -1;
+        
+        int res = 0;
+        int ander = pow(2,res);
+
+        while((m&ander) == (n&ander)) {
+            res++;
+            ander = pow(2,res);
+        }
+        return res+1;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+} 
+// } Driver Code Ends
