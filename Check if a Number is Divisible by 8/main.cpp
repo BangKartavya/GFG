ffb39cd8f8395d5b3af51dd9968be5a3bf123002
Kartavya Bang <bangkartavya@gmail.com>
2024-02-28 13:46:08 +0530

    02/28/2024

---
+++ Check if a Number is Divisible by 8/main.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int DivisibleByEight(string s){
+        
+        if(s.length()<=3) {
+            if(stoi(s)%8==0) return 1;
+            else return -1;
+        }
+        string lastThree = s.substr(s.length()-4,s.length());
+        
+        if(stoi(lastThree)%8==0) return 1;
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.DivisibleByEight(S)<<"\n";
+    }
+}
+// } Driver Code Ends
