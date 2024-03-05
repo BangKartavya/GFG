7c8a53507a126c38a55273c5f45a2e6767d80754
Kartavya Bang <bangkartavya@gmail.com>
2024-03-05 08:00:18 +0530

    03/05/2024

---
+++ Maximum Index/main.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int a[], int n) 
+    {
+        int maxi = 0;
+        int s = 0;
+        int e = n-1;
+        
+        while(s<=e) {
+            if(a[s]>a[e]) e--;
+            else {
+                maxi = max(maxi,e-s);
+                e = n-1;
+                s++;
+            }
+        }
+        
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
+        Solution ob;
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
+    }
+    return 0;
+} 
+// } Driver Code Ends
