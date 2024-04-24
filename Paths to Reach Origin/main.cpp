37f029d21318b17f98e4b6fded39a4ab749a4243
Kartavya Bang <bangkartavya@gmail.com>
2024-04-24 08:16:02 +0530

    04/24/2024

---
+++ Paths to Reach Origin/main.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    int ways(int x, int y)
+    {
+        vector<int> xx(x+1,1);
+        int MOD = 1e9+7;
+        
+        while(y--) for(int i = x-1;i>=0;i--) xx[i] = (xx[i]+xx[i+1])%MOD;
+        
+        return xx[0];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        Solution ob;
+        cout<<ob.ways(x,y)<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
