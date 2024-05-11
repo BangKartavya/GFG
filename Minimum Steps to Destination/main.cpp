6a8ecb54a859ea85437af1a6f3bb268c59e910e0
Kartavya Bang <bangkartavya@gmail.com>
2024-05-12 02:17:50 +0530

    05/12/2024

---
+++ Minimum Steps to Destination/main.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minSteps(int target) {
+        int sum = 0;
+        int step = 0;
+        
+        while(sum<target) {
+            step++;
+            sum+=step;
+        }
+        
+        while((sum-target)&1) {
+            sum-=(2*(sum-target));
+            step++;
+            sum+=step;
+        }
+        return step;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int d;
+        cin >> d;
+
+        Solution ob;
+        cout << ob.minSteps(d) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
