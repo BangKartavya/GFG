ac515a2667385bf7bf41ed2ed8e767f4ebae53eb
Kartavya Bang <bangkartavya@gmail.com>
2024-06-14 10:25:26 +0530

    06/14/2024

---
+++ Armstrong Numbers/main.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string armstrongNumber(int n){
+        int ans = 0;
+        int temp = n;
+        while(temp>0) {
+            int d = temp%10;
+            ans += d*d*d;
+            temp/=10;
+        }
+        return ans==n ? "Yes" : "No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
