0668701edd4112c22d16c23f885557186cb58821
Kartavya Bang <bangkartavya@gmail.com>
2024-07-16 10:24:35 +0530

    07/16/2024

---
+++ Remaining String/main.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string printString(string s, char ch, int count) {
+        int i = 0;
+        
+        while(i<s.size()) {
+            if(count == 0) return s.substr(i,s.size());
+            if(s[i] == ch) {
+                count--;
+            }
+            i++;
+        }
+        return "";
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        char ch;
+        int count;
+
+        cin >> s >> ch >> count;
+        Solution ob;
+        cout << ob.printString(s, ch, count) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
