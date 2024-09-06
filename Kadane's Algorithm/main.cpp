942bad7359b1ee72648175ceb851aa63e2ed96f5
Kartavya Bang <bangkartavya@gmail.com>
2024-09-06 10:31:07 +0530

    09/06/2024

---
+++ Kadane's Algorithm/main.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(vector<int> &arr) {
+        long long sum = INT_MIN;
+        long long ans = INT_MIN;
+        
+        for(auto i: arr) {
+            if(sum < 0) {
+                sum = i;
+            }
+            else sum += i;
+            
+            ans = max(sum,ans);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.maxSubarraySum(arr) << endl;
+    }
+}
+// } Driver Code Ends
