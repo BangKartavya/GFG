1fcd865700d6758048e4a16f68a546ad56d8428f
Kartavya Bang <bangkartavya@gmail.com>
2024-09-07 07:53:11 +0530

    09/07/2024

---
+++ Nth Natural Number/main.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long findNth(long long n) {
+        
+        if(n < 9) return n;
+        
+        return findNth(n/9)*10 + n%9;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, ans;
+        cin >> n;
+        Solution obj;
+        ans = obj.findNth(n);
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
