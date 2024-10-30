4648a0ceedd64be6ed8827cf776dbbdbc36ff5a6
Kartavya Bang <bangkartavya@gmail.com>
2024-10-30 15:51:09 +0530

    10/30/2024

---
+++ Pairs with Difference k/main.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    /* Returns count of pairs with difference k  */
+    int countPairsWithDiffK(vector<int>& arr, int k) {
+        int cnt = 0;
+        
+        unordered_map<int,int> mp;
+    
+        for(auto i: arr) mp[i]++;
+        
+        
+        for(auto i: arr) {
+            int t = i + k;
+            
+            if(mp[t] > 0) cnt += mp[t];
+        }
+        
+        
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        auto ans = ob.countPairsWithDiffK(arr, k);
+        cout << ans << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
