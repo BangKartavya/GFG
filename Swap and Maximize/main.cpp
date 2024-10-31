6cbafb447926fc8815b0ef5e1c3cbe0b4c3e63cf
Kartavya Bang <bangkartavya@gmail.com>
2024-11-01 03:12:53 +0530

    11/01/2024

---
+++ Swap and Maximize/main.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// long long int maxSum(int arr[], int n);
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long maxSum(vector<int>& arr) {
+        int n = arr.size(), res = 0; vector<int> newArr;
+        int i = 0, j = arr.size()-1; sort(arr.begin(),arr.end());
+        while ( i <= j ){
+            if ( i == j ){
+                newArr.push_back(arr[i]);
+                break;
+            } newArr.push_back(arr[i]); newArr.push_back(arr[j]);
+            i++; j--;
+        } 
+        for ( int i = 1; i<n; i++ ) res += abs(newArr[i]-newArr[i-1]);
+        return res + abs( newArr[n-1] - newArr[0] );
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To ignore any newline character left in the buffer
+    while (t--) {
+        string line;
+        getline(cin, line); // Read the entire line of input
+        stringstream ss(line);
+        vector<int> arr;
+        int num;
+
+        // Parse the input line into integers and add to the vector
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+
+        Solution ob;
+        long long ans = ob.maxSum(arr);
+
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
