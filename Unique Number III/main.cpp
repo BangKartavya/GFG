01027b67c4ec576f848eaa99a1e9c21e49c83f71
Kartavya Bang <bangkartavya@gmail.com>
2025-04-24 01:49:59 +0530

    04/24/2025

---
+++ Unique Number III/main.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int getSingle(vector<int> &arr) {
+        int ans=0;
+        int nm=1;
+        for(int i=0;i<32;i++){
+            int cb=0;
+            for(int x:arr) {
+                if(x&nm) cb++;
+            }
+            ans+=nm*(cb%3);
+            nm*=2;
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.getSingle(arr);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
