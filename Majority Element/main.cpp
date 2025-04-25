72a17f502abb38606a25a16e74553a69597fc258
Kartavya Bang <bangkartavya@gmail.com>
2025-04-25 07:22:27 +0530

    04/25/2025

---
+++ Majority Element/main.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    int majorityElement(vector<int>& arr) {
+        int elem = arr[0];
+        int cnt = 1;
+        int n = arr.size();
+        
+        for(int i = 1;i<n;i++) {
+            if(arr[i] == elem) {
+                cnt++;
+            }
+            else {
+                cnt--;
+                if(cnt == 0) {
+                    elem = arr[i];
+                    cnt = 1;
+                }
+            }
+        }
+        
+        cnt = 0;
+        
+        for(int i = 0;i<n;i++) cnt += (elem == arr[i]);
+        
+        if(cnt >n/2) return elem;
+        
+        return -1;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n;
+        vector<int> a, b;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.majorityElement(a) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
