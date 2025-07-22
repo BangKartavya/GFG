9c2aa54b6c17711f06b1e3ceecdcdc4360e94687
Kartavya Bang <bangkartavya@gmail.com>
2025-07-22 08:38:19 +0530

    07/22/2025

---
+++ Smallest Positive Missing/main.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    public:
+        int missingNumber(vector<int>& arr) {
+            // code here
+            int n = arr.size();
+
+            for(int i = 0; i < n; i++) {
+                if(arr[i] <= 0 || arr[i] > n) {
+                    arr[i] = n + 1;
+                }
+            }
+
+            for(int i = 0; i < n; i++) {
+                int num = abs(arr[i]);
+
+                if(num <= n) {
+                    arr[num - 1] = -abs(arr[num - 1]);
+                }
+            }
+
+            for(int i = 0; i < n; i++) {
+                if(arr[i] > 0) return i + 1;
+            }
+
+            return n + 1;
+        }
+};
