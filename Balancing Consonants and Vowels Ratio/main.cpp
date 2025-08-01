fcc973b017dc1f7a9e9e0f92be71f1e548685fa1
Kartavya Bang <bangkartavya@gmail.com>
2025-08-01 08:19:12 +0530

    08/01/2025

---
+++ Balancing Consonants and Vowels Ratio/main.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    private:
+        int help(string& s) {
+            int cnt = 0;
+            for(auto it : s) {
+                if(it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u') {
+                    cnt++;
+                }
+            }
+            return cnt;
+        }
+
+    public:
+        int countBalanced(vector<string>& arr) {
+            // code here
+            int n = arr.size();
+            vector<int> diff(n, 0);
+            for(int i = 0; i < n; i++) {
+                int vows = help(arr[i]);
+                diff[i] = 2 * vows - arr[i].size();
+            }
+            unordered_map<int, int> mp;
+            mp[0] = 1;
+            int sum = 0;
+            int ans = 0;
+            for(int i = 0; i < n; i++) {
+                sum += diff[i];
+                if(mp.find(sum) != mp.end()) {
+                    ans += mp[sum];
+                }
+                mp[sum]++;
+            }
+            return ans;
+        }
+};
