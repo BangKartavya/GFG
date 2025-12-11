84a97f6a65d72f9874bf69cbd4494363537e6052
Kartavya Bang <bangkartavya@gmail.com>
2025-12-11 09:42:49 +0530

    12/11/2025

---
+++ Construct an array from its pair-sum array/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+        vector<int> constructArr(vector<int>& arr) {
+            // code here
+            int n = (int)(sqrt(arr.size() * 2)) + 1;
+            vector<int> res(n);
+
+            res[0] = (arr[0] + arr[1] - arr[n - 1]) / 2;
+
+            for(int i = 1; i < n; i++) {
+                res[i] = (arr[i - 1] - res[0]);
+            }
+
+            // cout << "res : ";
+            // for(int& it: res) cout << it << " ";
+            // cout << endl;
+
+            return res;
+        }
+};
